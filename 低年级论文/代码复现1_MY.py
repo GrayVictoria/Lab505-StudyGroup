0d6c0cc8375e585288a7a9aac71236f7a2a20bcc
Gray <grayvictoria@sina.com>
2023-03-27 18:05:16 +0800

    Update 代码复现1_MY.py

--- 低年级论文/代码复现1_MY.py
+++ 低年级论文/代码复现1_MY.py
@@ -1,1 +1,4 @@
+### 贴对应的论文笔记链接！！！
+### https://github.com/GrayVictoria/Lab505-StudyGroup/blob/main/%E4%BD%8E%E5%B9%B4%E7%BA%A7%E8%AE%BA%E6%96%87/%E8%AE%BA%E6%96%87%E9%98%85%E8%AF%BB1_MY.md
+
 import xxx
